e4f9dd64ded475ddaa37f3ac7f32357f5c5840a4
shubham88fru <shubham88fru@yahoo.com>
2021-07-11 19:05:41 +0530

    context system in react

---
+++ src/components/Button.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import LanguageContext from '../context/LanguageContext';
+import ColorContext from '../context/ColorContext';
+
+class Button extends React.Component {
+  renderSubmit(language) {
+    return language === 'english' ? 'Submit' : 'Voorleggen';
+  }
+
+  renderButton(color) {
+    return (
+      <button className={`ui button ${color}`}>
+        <LanguageContext.Consumer>
+          {({ language }) => this.renderSubmit(language)}
+        </LanguageContext.Consumer>
+      </button>
+    );
+  }
+
+  render() {
+    return (
+      <ColorContext.Consumer>
+        {(color) => this.renderButton(color)}
+      </ColorContext.Consumer>
+    );
+  }
+}
+
+export default Button;
